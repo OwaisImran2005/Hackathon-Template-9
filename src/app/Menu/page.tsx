37f55c3cc54f662d96abbc6503ec0bbfee9136ab
OwaisImran2005 <owaisimran55555@gmail.com>
2024-12-18 03:03:27 +0500

    Update page.tsx

--- src/app/Menu/page.tsx
+++ src/app/Menu/page.tsx
@@ -12,6 +12,7 @@ import menu4 from "../media/menu4.png"
 import menu5 from "../media/menu5.png"
 import logomenu from "../media/Logo_menu.png"
 import Footer from "../components/Footer/page"
+import Image from "next/image";
 
 
 
@@ -24,46 +25,46 @@ export default function Menu (){
                 <p className="bactxt absolute font-geistsans text-5xl text-white font-semibold pt-28 ">Our Menu</p>
                 <p className="bactxt2 absolute font-inter text-lg text-white flex">Home <MdKeyboardArrowRight className="arr absolute ml-14" /></p>
                 <p className="bactxt3 absolute font-inter text-lg text-[#FF9F0D] ">Menu</p>
-                <img src={bac.src} alt="" width={1680}/>
+                <Image src={bac.src} alt="" width={1680}/>
             </div>
 
 
 <div>
-    <img src={men1.src} alt="" width={380} className="ml-60 mt-24 absolute"/>
+    <Image src={men1.src} alt="" width={380} className="ml-60 mt-24 absolute"/>
         
     <div className="dshtxt">
         <p className="absolute font-geistsans text-4xl mt-8 font-semibold">Starter Menu</p>
         <div className="dshtxt2">
 
-    <img src={vegtxt1.src} alt="" width={600} className="mt-4"/>
-    <img src={vegtxt2.src} alt="" width={600} className="mt-4"/>
-    <img src={vegtxt3.src} alt="" width={600} className="mt-4"/>
-    <img src={vegtxt4.src} alt="" width={600} className="mt-4"/>
+    <Image src={vegtxt1.src} alt="" width={600} className="mt-4"/>
+    <Image src={vegtxt2.src} alt="" width={600} className="mt-4"/>
+    <Image src={vegtxt3.src} alt="" width={600} className="mt-4"/>
+    <Image src={vegtxt4.src} alt="" width={600} className="mt-4"/>
         </div>
     </div>
 </div>
 
 <div className="Menu_2">
-<img src={menu2.src} alt="" width={1100} />
+<Image src={menu2.src} alt="" width={1100} />
 </div>
 
 <div className="Menu_3">
 
-<img src={menu3.src} alt="" />
+<Image src={menu3.src} alt="" />
 </div>
 
 <div className="Menu_4">
-    <img src={menu4.src} alt="" width={1100} />
+    <Image src={menu4.src} alt="" width={1100} />
 </div>
 
 <div className="Menu_5">
-    <img src={menu5.src} alt="" width={1100} />
+    <Image src={menu5.src} alt="" width={1100} />
 </div>
 
 <div>
     <p className="font-inter text-lg font-extralight flex justify-center pt-24">Partners & Clients</p>
     <p className="font-geistsans text-[#333333] text-3xl font-semibold flex justify-center pt-4">We work with the best pepole</p>
-    <img src={logomenu.src} alt="" width={1100} className="pt-14 ml-60" />
+    <Image src={logomenu.src} alt="" width={1100} className="pt-14 ml-60" />
 </div>
 
 <Footer/>
