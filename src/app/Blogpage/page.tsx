31a2813c5a1ef8c9b42f48b5e4a975ab9e62fa1a
OwaisImran2005 <owaisimran55555@gmail.com>
2024-12-18 02:48:31 +0500

    Update page.tsx

--- src/app/Blogpage/page.tsx
+++ src/app/Blogpage/page.tsx
@@ -8,6 +8,8 @@ import blog4 from "../media/Blog4.png"
 import change from "../media/pagination.png"
 import sidebr from "../media/Side ber.png"
 import Footer from "../components/Footer/page";
+import Image from "next/image";
+
 
 export default function Blog(){
  
@@ -20,22 +22,22 @@ export default function Blog(){
                 <p className="bactxt absolute font-geistsans text-5xl text-white font-semibold pt-28">Blog page</p>
                 <p className="bactxt2 absolute font-inter text-lg text-white flex">Home <MdKeyboardArrowRight className="arr absolute ml-14" /></p>
                 <p className="bactxt3 absolute font-inter text-lg text-[#FF9F0D] ">Blog page</p>
-                <img src={bac.src} alt="" width={1680}/>
+                <Image src={bac.src} alt="" width={1680}/>
             </div>
 
 <div className="blogs mt-16 ">
-    <img src={blog1.src} alt="" width={700} className="pt-10"/>
-    <img src={blog2.src} alt="" width={700} className="pt-10"/>
-    <img src={blog3.src} alt="" width={700} className="pt-10"/>
-    <img src={blog4.src} alt="" width={700} className="pt-10"/>
+    <Image src={blog1.src} alt="" width={700} className="pt-10"/>
+    <Image src={blog2.src} alt="" width={700} className="pt-10"/>
+    <Image src={blog3.src} alt="" width={700} className="pt-10"/>
+    <Image src={blog4.src} alt="" width={700} className="pt-10"/>
 </div>
 
 <div className="sidebar">
-    <img src={sidebr.src} alt="" className="absolute" />
+    <Image src={sidebr.src} alt="" className="absolute" />
 </div>
 
 <div>
-    <img src={change.src} alt="" className="change" />
+    <Image src={change.src} alt="" className="change" />
 </div>
 
 
