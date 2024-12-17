60647699d6d8bfd8d9124715a2fe304fad469bdc
OwaisImran2005 <owaisimran55555@gmail.com>
2024-12-18 03:04:28 +0500

    Update page.tsx

--- src/app/Shoplist/page.tsx
+++ src/app/Shoplist/page.tsx
@@ -4,6 +4,7 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 import Footer from "../components/Footer/page";
 import shp1 from "../media/shop1.png"
 import shp2 from "../media/shop_2.png"
+import Image from "next/image";
 
 
 export default function Shop(){
@@ -15,15 +16,15 @@ export default function Shop(){
                 <p className="bactxt absolute font-geistsans text-5xl text-white font-semibold pt-28">Our Shop</p>
                 <p className="bactxt2 absolute font-inter text-lg text-white flex">Home <MdKeyboardArrowRight className="arr absolute ml-14" /></p>
                 <p className="bactxt3 absolute font-inter text-lg text-[#FF9F0D] ">Shop</p>
-                <img src={bac.src} alt="" width={1680}/>
+                <Image src={bac.src} alt="" width={1680}/>
             </div>
 
             <div className="shop1 mt-24 ">
-    <img src={shp1.src} alt="" width={730}/>
+    <Image src={shp1.src} alt="" width={730}/>
 </div>
 
 <div className="sidebar3">
-    <img src={shp2.src} alt="" className="" width={320} />
+    <Image src={shp2.src} alt="" className="" width={320} />
 </div>
 
      <Footer/>
