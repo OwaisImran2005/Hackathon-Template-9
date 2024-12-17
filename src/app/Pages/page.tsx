7e6a9f2771aadebdfd2ca4c3260ec6faf82a4ca4
OwaisImran2005 <owaisimran55555@gmail.com>
2024-12-18 03:04:01 +0500

    Update page.tsx

--- src/app/Pages/page.tsx
+++ src/app/Pages/page.tsx
@@ -5,6 +5,8 @@ import blogdtls from "../media/Left ber.png"
 import change from "../media/pagination.png"
 import sidebr from "../media/Side ber.png"
 import Footer from "../components/Footer/page";
+import Image from "next/image";
+
 
 export default function Pages(){
  
@@ -17,15 +19,15 @@ export default function Pages(){
                 <p className="bactxt absolute font-geistsans text-5xl text-white font-semibold pt-28">Blog details</p>
                 <p className="bactxt2 absolute font-inter text-lg text-white flex">Home <MdKeyboardArrowRight className="arr absolute ml-14" /></p>
                 <p className="bactxt3 absolute font-inter text-lg text-[#FF9F0D] ">Blog details</p>
-                <img src={bac.src} alt="" width={1680}/>
+                <Image src={bac.src} alt="" width={1680}/>
             </div>
 
 <div className="blogdtls mt-24 ">
-    <img src={blogdtls.src} alt="" width={700}/>
+    <Image src={blogdtls.src} alt="" width={700}/>
 </div>
 
 <div className="sidebar2">
-    <img src={sidebr.src} alt="" className="" />
+    <Image src={sidebr.src} alt="" className="" />
 </div>
 
 <Footer/>
